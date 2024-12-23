5a9200f39b0bb57fdb104b0018097638486de6f6
Rahul <amaleexxii@gmail.com>
2024-12-24 04:33:04 +0530

    changed Button Link to Navigate

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,25 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ThemeBtn from "../themeButton/ThemeBtn";
 import { apiDataContext } from "../accordian/AccordianContext";
 import { useContext } from "react";
 
 const Navbar = () => {
   
+  const navigate = useNavigate();
   const style = "bg-lime-400 border-2 rounded p-2 font-semibold font-mono";
   const { refetch } = useContext(apiDataContext);
 
   const handleGetFetch = async () => {
     await refetch();
+    /* Link is not suppported asynchronous operations so we need to use Navigate */
+    navigate("/accordian");
   };
 
   return (
     <div className="h-16 bg-orange-300 flex justify-between p-4 gap-3">
-      {/* Best way to do using Navigate hook insted of Link bcz Link doesnt allow asynchronous opereations */}
-      <Link to="/accordian">
-        <button onClick={handleGetFetch} className={style}>
-          Accordian
-        </button>
-      </Link>
+      <button onClick={handleGetFetch} className={style}>
+        Accordian
+      </button>
+
       <Link to="/parent1">
         <button className={style}>parent1 </button>
       </Link>
