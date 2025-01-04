5110f67232b5e941a513365a6bb7edddceb00bae
Rahul <amaleexxii@gmail.com>
2025-01-04 08:57:38 +0530

    removed unwanted data

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react"
 import Button from "./Button"
-import { Link } from "react-router-dom"
-import { ArrowBigRightDash, LucideAArrowUp } from "lucide-react"
 
 
 function Card () {
@@ -13,14 +11,6 @@ function Card () {
             <h1>Count : { count }</h1>
             <Button count={count} setCount={setCount}/>
         </div>
-
-        {/* <Link to='accordian'>
-        <div className="flex justify-end mt-40">
-            Next
-                <ArrowBigRightDash fill="yellow"/> 
-            
-        </div>
-        </Link> */}
         </>
     )
 }
